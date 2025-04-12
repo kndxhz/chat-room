929830424ccb11db3a614dbaf8b12a4b55bc8689
kndxhz <kndxhz@163.com>
2025-04-12 23:45:39 +0800

    删除无用的批处理文件，清理项目结构

--- 1.启动后端.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-title Æô¶¯ºó¶Ë
-.\python-3.13.2-embed-amd64\python.exe main.py
--- 2.启动前端.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title 启动前端
-echo 已经启动在 http://127.0.0.1
-cd nginx-1.27.4
-.\nginx.exe -g "daemon off;"
-taskkill /f /im nginx.exe
